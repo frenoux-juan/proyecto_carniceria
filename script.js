50b0e53713c818d9ff5292ba5aa23d9fe7a02bd8
frenoux-juan <88634861+frenoux-juan@users.noreply.github.com>
2023-10-13 16:00:20 -0300

    reutilizacion de cabecera y pie de pagina utilizando javascript terminado

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Encabezado
+    const headerContainer = document.getElementById('header-container');
+    const headerRequest = new XMLHttpRequest();
+    headerRequest.open('GET', 'header.html', true);
+    headerRequest.onreadystatechange = function () {
+        if (headerRequest.readyState === 4 && headerRequest.status === 200) {
+            headerContainer.innerHTML = headerRequest.responseText;
+        }
+    };
+    headerRequest.send();
+
+    // Pie de página
+    const footerContainer = document.getElementById('footer-container');
+    const footerRequest = new XMLHttpRequest();
+    footerRequest.open('GET', 'footer.html', true);
+    footerRequest.onreadystatechange = function () {
+        if (footerRequest.readyState === 4 && footerRequest.status === 200) {
+            footerContainer.innerHTML = footerRequest.responseText;
+        }
+    };
+    footerRequest.send();
+});
